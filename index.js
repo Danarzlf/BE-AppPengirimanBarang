77ba016fcdbc4abb35743515d42a1ece5d9c4a35
Danar Zulfian Wirakusumah <127115867+Danarzlf@users.noreply.github.com>
2024-06-11 09:29:59 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,63 +3,60 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
 const userRoute = require("./routes/user.routes");
 const userProfileRoute = require("./routes/userProfile.routes");
+const shipmentRoute = require("./routes/shipment.routes");
+const senderRoute = require("./routes/sender.routes");
 
-const app = express(); // Pastikan app diinisialisasi sebelum digunakan
+const app = express();
 
-// Konfigurasi CORS
-const corsConfig = {
-  origin: "*",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-};
 
-app.options("*", cors(corsConfig)); // Gunakan "*" untuk mengizinkan semua rute
-app.use(cors(corsConfig));
-
-// Middleware
 app.use(express.json());
+app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-// Rute
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/user-profiles", userProfileRoute);
+app.use("/api/v1/shipments", shipmentRoute);
+app.use("/api/v1/senders", senderRoute);
+
 
 app.get("/", (req, res) => {
-  res.send("Welcome to server");
-});
+    res.send("Welcome to server");
+  });
+  
 
 // 404 error handling
 app.use((req, res, next) => {
-  res.status(404).json({
-    status: false,
-    message: "Bad Request",
-    data: null,
+    res.status(404).json({
+      status: false,
+      message: "Bad Request",
+      data: null,
+    });
   });
-});
-
-// 500 error handling
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    status: false,
-    message: err.message ?? "Internal Server Error",
-    data: null,
+  
+  // 500 error handling
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      status: false,
+      message: err.message ?? "Internal Server Error",
+      data: null,
+    });
   });
-});
+  
 
 const PORT = process.env.PORT || 7000;
 const MONGOURL = process.env.MONGO_URL;
 
 mongoose
-  .connect(MONGOURL, { useNewUrlParser: true, useUnifiedTopology: true }) // Menambahkan opsi untuk menghindari peringatan deprecation
-  .then(() => {
-    console.log("Database is connected successfully.");
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.log(error));
+    .connect(MONGOURL)
+    .then(() => {
+        console.log("Database is connected successfully.");
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => console.log(error));
